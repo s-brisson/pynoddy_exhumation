8aad5b3a526af7d6b25087be626e1fb074ac35bc
s-brisson <66142009+s-brisson@users.noreply.github.com>
2023-09-13 10:06:10 +0200

    Update execute_mcmc_new.sh

--- ClusterSubmission/execute_mcmc_new.sh
+++ ClusterSubmission/execute_mcmc_new.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd /home/ho640525/projects/Exhumation
-source /home/ho640525/.cache/pypoetry/virtualenvs/exhumation-IrWm4u2d-py3.8/bin/activate
-export PYTHONPATH=$PYTHONPATH:/home/ho640525/projects/pynoddy/pynoddy
-python case_mcmc_new.py $1 $2 $3 $4 --folder $5 
+std_values = (500 500 1000 300)
+
+for std in "${std_values[@]}"; do
+  cd /home/ho640525/projects/Exhumation
+  source /home/ho640525/.cache/pypoetry/virtualenvs/exhumation-IrWm4u2d-py3.8/bin/activate
+  export PYTHONPATH=$PYTHONPATH:/home/ho640525/projects/pynoddy/pynoddy
+  python case_mcmc_new.py "$1" "$2" --property "$3" --standard_deviation "$std" --folder "$5" 
+done
