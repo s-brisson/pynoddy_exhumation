c8729beada7fd0b082dc9b20b1471c5bdbcfd244
s-brisson <66142009+s-brisson@users.noreply.github.com>
2023-09-18 15:56:38 +0200

    Update execute_exh_block.sh

--- ClusterSubmission/execute_exh_block.sh
+++ ClusterSubmission/execute_exh_block.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-#cd /home/ho640525/projects/Exhumation
-#source /home/ho640525/.cache/pypoetry/virtualenvs/exhumation-IrWm4u2d-py3.8/bin/activate
-#export PYTHONPATH=$PYTHONPATH:/home/ho640525/projects/pynoddy/pynoddy
-#python exhumation_block.py $1 $2 $3 --folder $4 
-
-#!/bin/bash
-
 cd /home/ho640525/projects/Exhumation
 source /home/ho640525/.cache/pypoetry/virtualenvs/exhumation-IrWm4u2d-py3.8/bin/activate
 export PYTHONPATH=$PYTHONPATH:/home/ho640525/projects/pynoddy/pynoddy
