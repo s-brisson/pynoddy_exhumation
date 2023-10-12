2a9d29d0cac157d4d8221e7c09a3738f79fb5b18
s-brisson <66142009+s-brisson@users.noreply.github.com>
2023-10-12 09:25:39 +0200

    Delete ClusterSubmission/execute_no_mcmc.sh

--- ClusterSubmission/execute_no_mcmc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/ho640525/projects/Exhumation
-source /home/ho640525/.cache/pypoetry/virtualenvs/exhumation-IrWm4u2d-py3.8/bin/activate
-export PYTHONPATH=$PYTHONPATH:/home/ho640525/projects/pynoddy/pynoddy
-python case_no_mcmc.py $1 --folder $2 
