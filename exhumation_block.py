cb241f2879f6928dc0a041b29b69646e1ced9b07
s-brisson <66142009+s-brisson@users.noreply.github.com>
2023-10-25 09:49:16 +0200

    Update exhumation_block.py

--- exhumation_block.py
+++ exhumation_block.py
@@ -77,12 +77,12 @@ for i in range(len(all_params)):
             hist_copy.events[fault].properties[prop] = all_params[i][p+f*2]
             
     model_coords,_,_ = exhumationComplex(i,hist_copy, lith, res, interval, upperlim, label) 
-    exh_block,_,raw_exh_block = exhumation_grid_single(model_coords, out_hd, res, zdim)
+    exh_block,_,_ = exhumation_grid_single(model_coords, out_hd, res, zdim)
 
-    with open(f'{model_rawdata_folder}/rawblock_{label}_row{i}.pkl', 'wb') as f:
-        pickle.dump(raw_exh_block, f)
-    with open(f'{model_rawdata_folder}/rawcoords_{label}_row{i}.pkl', 'wb') as k:
-        pickle.dump(model_coords, k)
+    #with open(f'{model_rawdata_folder}/rawblock_{label}_row{i}.pkl', 'wb') as f:
+    #    pickle.dump(raw_exh_block, f)
+    #with open(f'{model_rawdata_folder}/rawcoords_{label}_row{i}.pkl', 'wb') as k:
+    #    pickle.dump(model_coords, k)
     np.save(f"{model_exhumation_folder}/exh_block_{label}_row{i}.npy", exh_block)
     np.save(f"{model_params_folder}/params_{label}_row{i}.npy", all_params[i])
 
