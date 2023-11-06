b1b7e2cc81ca0e0a145d463e299f9155d5bc9514
s-brisson <66142009+s-brisson@users.noreply.github.com>
2023-11-06 16:40:39 +0100

    Update execute_mcmc_new.sh

--- ClusterSubmission/execute_mcmc_new.sh
+++ ClusterSubmission/execute_mcmc_new.sh
@@ -3,4 +3,4 @@
 cd /home/ho640525/projects/Exhumation
 source /home/ho640525/.cache/pypoetry/virtualenvs/exhumation-IrWm4u2d-py3.8/bin/activate
 export PYTHONPATH=$PYTHONPATH:/home/ho640525/projects/pynoddy/pynoddy
-python case_mcmc_new.py "$1" --folder "$2" 
+python exh_average.py "$1" --folder "$2" 
