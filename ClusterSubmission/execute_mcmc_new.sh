ad0460c4cc465f4e19a78d2f96ff606d0ef78dad
s-brisson <66142009+s-brisson@users.noreply.github.com>
2023-12-20 17:39:31 +0100

    Update execute_mcmc_new.sh

--- ClusterSubmission/execute_mcmc_new.sh
+++ ClusterSubmission/execute_mcmc_new.sh
@@ -3,4 +3,4 @@
 cd /home/ho640525/projects/Exhumation
 source /home/ho640525/.cache/pypoetry/virtualenvs/exhumation-IrWm4u2d-py3.8/bin/activate
 export PYTHONPATH=$PYTHONPATH:/home/ho640525/projects/pynoddy/pynoddy
-python model_selection.py "$1" --folder "$2" 
+python sensitivity.py "$1" --folder "$2" 
