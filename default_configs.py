5258cd93ca32b57a2845e9cd7026b5951a745bfb
s-brisson <66142009+s-brisson@users.noreply.github.com>
2024-04-05 17:54:33 +0200

    Update default_configs.py

--- default_configs.py
+++ default_configs.py
@@ -2,7 +2,7 @@ import numpy as np
 #### INPUTS
 history = 'data/input_files/bregenz_ver5.his'
 all_params = 'data/input_files/training_params_310.npy'
-history_samples = 'data/input_files/bregenz_samples.his'
+history_samples = 'data/input_files/schuller_samples.his'
 samples = 'data/input_files/bregenz_data.csv'
 noddy_exe = "/rwthfs/rz/cluster/home/ho640525/projects/pynoddy/pynoddy/noddyapp/noddy"
 save_each = False
@@ -15,7 +15,7 @@ lith = [8] #lith ID for the indicator layer.
 
 #### OUTPUTS
 output_folder = "data/outputs"
-model_name = "transalp"
+model_name = "schuller"
 
 maps = [[21,-1000,-11000],
        [22,2000,-9500],
@@ -23,8 +23,11 @@ maps = [[21,-1000,-11000],
 synthetic_data = [[21, 3400],[22, 3300],[24,5400]]
 sigma = 800
 
-#INPUTS FOR MCMC (without indicator layer)
-sample_num = [4] #sample indices
+#INPUTS Subalpine Molasse model selection
+sample_num = [0,1,2,3,4] #sample indices
+event = [13,14,15,16,17]
+lith_list = [11,12,13,14,15]
+std = [500,2000,2000,1000]
 
 #TRANSALP INPUTS
 history_transalp = 'data/input_files/trans005.his'
@@ -32,6 +35,6 @@ synth_samples = 'data/input_files/synth_samples_model2.csv'
 avg_conv_factor = ((7500/3372.5) + (4602/2041.25)+ (9999/4437.5)) / 3
 geo_gradient = 25
 prop = ['X', 'Z', 'Amplitude','Slip'] #properties to modify
-event = np.arange(22,31) #events to modify
-std = [2000,800,1000,600] #
-lith_list = np.arange(16,28) #lith IDs for the samples being analyzed.
+#event = np.arange(22,31) #events to modify
+#std = [2000,800,1000,600] #
+#lith_list = np.arange(16,28) #lith IDs for the samples being analyzed.
