0ce8a7774342a6025b17e992e77c09b73d4922fa
s-brisson <66142009+s-brisson@users.noreply.github.com>
2024-04-08 11:57:28 +0200

    Update default_configs.py

--- default_configs.py
+++ default_configs.py
@@ -15,7 +15,7 @@ lith = [8] #lith ID for the indicator layer.
 
 #### OUTPUTS
 output_folder = "data/outputs"
-model_name = "schuller"
+model_name = "ortner"
 
 maps = [[21,-1000,-11000],
        [22,2000,-9500],
@@ -34,7 +34,7 @@ history_transalp = 'data/input_files/trans005.his'
 synth_samples = 'data/input_files/synth_samples_model2.csv'
 avg_conv_factor = ((7500/3372.5) + (4602/2041.25)+ (9999/4437.5)) / 3
 geo_gradient = 25
-prop = ['X','Z', 'Amplitude','Slip'] #properties to modify
+prop = ['Z', 'Amplitude','Slip'] #properties to modify
 #event = np.arange(22,31) #events to modify
 #std = [2000,800,1000,600] #
 #lith_list = np.arange(16,28) #lith IDs for the samples being analyzed.
