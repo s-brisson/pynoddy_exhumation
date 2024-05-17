d8ae4a2b47634377574bab1168206e90a6975578
s-brisson <66142009+s-brisson@users.noreply.github.com>
2024-05-17 17:57:45 +0200

    Update exh_functions.py

--- exh_functions.py
+++ exh_functions.py
@@ -208,7 +208,7 @@ def exhumationComplex(ndraw, history, lith, res = 8, interval = 50, upperlim = 0
         
         for j in range(len(x)):    
             #coords.append([n_sample,x[j],y[j],z[j],exhumation[j],ndraw])
-            coords.append([x[j],y[j],z[j],exhumation[j]])
+            coords.append([n_sample,x[j],y[j],z[j],exhumation[j]])
         
         #coords = np.array(coords)
             
